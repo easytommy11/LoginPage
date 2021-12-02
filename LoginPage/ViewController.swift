dc5ebf228ad5b89d0b63df40ae03a738d66bc96f
Tomiwa Babalola <tbabalola004@gmail.com>
2021-12-02 09:33:00 -0500

    fghj

--- LoginPage/ViewController.swift
+++ LoginPage/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+    }
+    
+
 
 
 }
